815d8542d4ee55c2cf4c8b8193dcaefdfa6217ed
HanB312 <70627208+HanB312@users.noreply.github.com>
2025-06-27 02:44:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 from flask import Flask, render_template, redirect, url_for, session, request, jsonify
 from models import init_db, get_db
